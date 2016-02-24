0df0559c9ba6e8c1adc7d22673ba5746896fbeb4
Taris Atram <taris.atram@outlook.com>
2016-02-23 23:04:03 -0500

    Allow net_connect to codeclimate.com

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'webmock/rspec'
 require 'omniauth'
 require 'omniauth-crest'
 
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.include WebMock::API
